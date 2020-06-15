01e484bdfe6084b0b8d0c5188ff133435fc154af
Joe Bloggs <jezproject@gmail.com>
2020-06-15 08:54:13 +0100

    Now rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,12 @@
 def main():
-  print('You rolled a die')
+  import random
+  dice_rolls = 2
+  dice_sum = 0
+  for i in range(0, dice_rolls): ###for every index in a range starting after 0 until the value is equal to dice_rolls, do this:
+    roll = random.randint(1,6)
+    dice_sum += roll
+    print(f'You rolled a {roll}')
+  print(f"You rolled a total {dice_sum}")  
 
 if __name__== "__main__":
   main()
